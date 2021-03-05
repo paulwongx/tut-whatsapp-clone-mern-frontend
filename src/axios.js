890bab39167f60f94137f9276944366c09dbf346
paulwongx <20178251+paulwongx@users.noreply.github.com>
2021-03-04 23:07:06 -0500

    Removed forced headers

--- src/axios.js
+++ src/axios.js
@@ -2,9 +2,6 @@ import axios from 'axios';
 
 const instance = axios.create({
 	baseURL: 'https://tut-whatsapp-clone-mern.herokuapp.com/',
-    headers: {
-        'Referer': window.location.origin
-    }
 });
 
 export default instance;
