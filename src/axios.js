65e120d7d3db0f808ec276e712157a3d31c76445
paulwongx <20178251+paulwongx@users.noreply.github.com>
2021-03-04 22:49:42 -0500

    Added withCredentials in Axios default

--- src/axios.js
+++ src/axios.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
 	baseURL: 'https://tut-whatsapp-clone-mern.herokuapp.com/',
+    withCredentials: true,
 });
 
 export default instance;
