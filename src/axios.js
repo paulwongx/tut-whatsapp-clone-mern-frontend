525eb3f00676fec851a1283a3e31a0b1424f2729
paulwongx <20178251+paulwongx@users.noreply.github.com>
2021-03-04 23:04:19 -0500

    Added referer header

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 const instance = axios.create({
 	baseURL: 'https://tut-whatsapp-clone-mern.herokuapp.com/',
-    withCredentials: true,
+    headers: {
+        'Referer': window.location.origin
+    }
 });
 
 export default instance;
